991572fcbed7f438c13b0bed445e8293476f64fd
caioragazzini <caiovrragazzini@gmail.com>
2022-03-12 14:36:51 -0400

    Create Model Classes Truck

---
+++ BackEnd/CRUD-TRUCK/CRUD-TRUCK/Models/Truck.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CRUD_TRUCK.Models
+{
+    [Table("Trucks")]
+    public class Truck
+    {
+        [Key]
+        public int TruckId { get; set; }
+        public string NameTruck { get; set; }
+        public int YearOfManufacture { get; set; }
+        public int YearModel { get; set; }
+        public TruckModel TruckModels { get; set; }
+        public int TruckModelId { get; set; }
+
+    }
+}
---
+++ BackEnd/CRUD-TRUCK/CRUD-TRUCK/Models/TruckModel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CRUD_TRUCK.Models
+{
+    [Table("TruckModel")]
+    public class TruckModel
+    {
+        public TruckModel()
+        {
+            Trucks = new Collection<Truck>();
+        }
+
+        [Key]
+        public int TruckModelId { get; set; }
+
+        [Required]
+        [MaxLength(2)]
+        public string TruckModelType { get; set; }
+
+        public ICollection<Truck> Trucks { get; set; }
+
+    }
+}
